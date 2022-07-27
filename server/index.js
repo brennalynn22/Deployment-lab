371976e1ce72803de793fc5c532a6989a682e3ed
Breezy Wentz <brwentz@gmail.com>
2022-07-27 14:40:42 -0500

    initial deployment

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express()
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/styles.css'))
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/main.js'))
+})
+
+    const port = process.env.PORT || 4005
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)})
+
