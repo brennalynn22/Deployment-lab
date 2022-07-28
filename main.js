59b384632ebfa169f6086e958dcd6b2abf5cbc48
Breezy Wentz <brwentz@gmail.com>
2022-07-28 14:03:33 -0500

    rollbar try catch

---
+++ main.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('button')
+
+button.addEventListener('click', sage())
+function sage(){
+    alert('Store coming soon! Sage is currently taste testing')
+}
+try {
+    nonExistentFunction();
+  } catch (error) {
+    console.error(error);
+    // expected output: ReferenceError: nonExistentFunction is not defined
+    // Note - error messages will vary depending on browser
+  }
+  fg
--- server/index.js
+++ server/index.js
@@ -2,6 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'd4f11e1dcac747d28d22c351e415c8ef',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const app = express()
 
 app.get('/', function(req, res){
